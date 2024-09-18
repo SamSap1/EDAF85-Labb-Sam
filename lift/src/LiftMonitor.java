8002945bbdcc97ce320949a1916dbc588863ea54
SamSap1 <150430881+SamSap1@users.noreply.github.com>
2024-09-18 16:21:32 +0200

    LAB 2: creation of monitor attributes

--- lift/src/LiftMonitor.java
+++ lift/src/LiftMonitor.java
@@ -1,5 +1,15 @@
+import lift.LiftView;
+
 public class LiftMonitor {
     private boolean isLiftFull;
+    private int currFloor;
+    private int destFloor;
+    private int [] priorityEntry;
+    private int [] priorityExit;
+    private boolean entering;
+    private boolean exiting;
+    private LiftView lv;
+    
 
 
     public LiftMonitor(){
