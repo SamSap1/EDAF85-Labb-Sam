6b3c7194d61b4b4ab09fb19f4efa9e0601fae959
HugoEklund <eklund.hugo@gmail.com>
2024-09-25 14:27:05 +0200

    uuh

--- lift/src/PassengerGenerator.java
+++ lift/src/PassengerGenerator.java
@@ -1,3 +1,49 @@
-public class PassengerGenerator {
+import lift.Passenger;
 
+public class PassengerGenerator implements Passenger
+{
+    private int startFloor;
+    private int destFloor;
+    private int floorCount;
+
+    public PassengerGenerator(int floorCount)
+    {
+
+    }
+
+    /** @return the floor the passenger starts at */
+    public int getStartFloor()
+    {
+        return startFloor;
+    }
+
+    /** @return the floor the passenger is going to */
+    public int getDestinationFloor()
+    {
+        return destFloor;
+    }
+
+    /** First, delay for 0..45 seconds. Then animate the passenger's walk, on the entry floor, to the lift. */
+    public void begin()
+    {
+
+    }
+
+    /** Animate the passenger's walk from the entry floor into the lift. */
+    public void enterLift()
+    {
+
+    }
+
+    /** Animate the passenger's walk out of the lift, to the exit floor. */
+    public void exitLift()
+    {
+
+    }
+
+    /** Animate the passenger's walk, on the exit floor, out of view. */
+    public void end()
+    {
+
+    }
 }
--- lift/src/PassengerThread.java
+++ lift/src/PassengerThread.java
@@ -1,5 +1,35 @@
+import lift.Passenger;
 
+public class PassengerThread extends Thread
+{
+    private PassengerGenerator p;
+    private LiftMonitor monitor;
 
-public class PassengerThread extends Thread{
+    public PassengerThread(LiftMonitor monitor, int floorCount)
+    {
+        this.monitor = monitor;
+        this.p = new PassengerGenerator(floorCount);
+    }
+
+    @Override
+    public void run()
+    {
+        try 
+        {
+            while(true)
+            {
+                p.begin();
+                int currentFloor = p.getStartFloor();
+                int nextFloor = p.getDestinationFloor();
+
+                monitor.enterLift(currentFloor);
+                p.enterLift();
+            }
+
+        } catch (Exception e) 
+        {
+            
+        }
+    }
 
 }
