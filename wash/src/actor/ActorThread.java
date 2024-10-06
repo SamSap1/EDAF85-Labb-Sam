c22038638e7b5e7b8c5134df907abab28d7e1d01
HugoEklund <eklund.hugo@gmail.com>
2024-10-06 19:53:25 +0200

    actorthread klar

--- wash/src/actor/ActorThread.java
+++ wash/src/actor/ActorThread.java
@@ -1,25 +1,26 @@
 package actor;
 
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 public abstract class ActorThread<M> extends Thread {
 
-    // TODO: one suitable attribute here
+    private final LinkedBlockingQueue<M> messageQueue = new LinkedBlockingQueue<>();
 
     /** Called by another thread, to send a message to this thread. */
     public void send(M message) {
-        // TODO: implement this method (one or a few lines)
+        messageQueue.offer(message);
     }
     
     /** Returns the first message in the queue, or blocks if none available. */
     protected M receive() throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+        return messageQueue.take();
     }
     
     /** Returns the first message in the queue, or blocks up to 'timeout'
         milliseconds if none available. Returns null if no message is obtained
         within 'timeout' milliseconds. */
     protected M receiveWithTimeout(long timeout) throws InterruptedException {
-        // TODO: implement this method (one or a few lines)
-        return null;
+        return messageQueue.poll(timeout, TimeUnit.MILLISECONDS);
     }
 }
