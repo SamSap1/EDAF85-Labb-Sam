626261406e9126ef2cbba26c2b426437c3f7686a
SamSap1 <150430881+SamSap1@users.noreply.github.com>
2024-10-06 21:51:10 +0200

    Wash Developed + WaterController starter

--- wash/src/wash/control/Wash.java
+++ wash/src/wash/control/Wash.java
@@ -14,6 +14,7 @@ public class Wash {
         ActorThread<WashingMessage> temp = new TemperatureController(io);
         ActorThread<WashingMessage> water = new WaterController(io);
         ActorThread<WashingMessage> spin = new SpinController(io);
+        //WashingProgram3 wp3;
 
         temp.start();
         water.start();
@@ -23,7 +24,22 @@ public class Wash {
             int n = io.awaitButton();
             System.out.println("user selected program " + n);
 
+            switch (n) {
+                case 1:
+                break;
+
+                case 2:
+                break;
+
+                case 3:
+                   WashingProgram3 wp3 = new WashingProgram3(io, temp, water, spin);
+                   wp3.start();
+                    break;
             
+                default:
+                    break;
+            }                    
+
             
             // TODO:
             // if the user presses buttons 1-3, start a washing program
--- wash/src/wash/control/WaterController.java
+++ wash/src/wash/control/WaterController.java
@@ -1,5 +1,7 @@
 package wash.control;
 
+import static wash.control.WashingMessage.Order.ACKNOWLEDGMENT;
+
 import actor.ActorThread;
 import wash.io.WashingIO;
 
@@ -7,13 +9,19 @@ public class WaterController extends ActorThread<WashingMessage> {
 
     // TODO: add attributes
     private WashingIO io;
+    private double inputFlow = 0.1;
+    private double outputFlow = 0.2;
+    private ActorThread<WashingMessage> send;
+    private double WaterLevel;
+
     
+
 
 
 
     public WaterController(WashingIO io) {
         // TODO
-
+        this.io = io;
 
 
     }
@@ -21,5 +29,29 @@ public class WaterController extends ActorThread<WashingMessage> {
     @Override
     public void run() {
         // TODO
+        try {
+            while (true){
+                WashingMessage wm = receiveWithTimeout(60000 / Settings.SPEEDUP);
+
+                    wm.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+
+                    if (wm != null){
+
+                        
+
+
+
+                    }
+                
+
+
+            }
+
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+
     }
 }
