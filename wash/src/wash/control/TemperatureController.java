0989c5190b315c52f6ea8a918a8f68d3a3a4393d
SamSap1 <150430881+SamSap1@users.noreply.github.com>
2024-10-07 18:14:14 +0200

    Update TemperatureController.java

--- wash/src/wash/control/TemperatureController.java
+++ wash/src/wash/control/TemperatureController.java
@@ -3,16 +3,42 @@ package wash.control;
 import actor.ActorThread;
 import wash.io.WashingIO;
 
+
+
+
 public class TemperatureController extends ActorThread<WashingMessage> {
+    private final double margin;
+    private final WashingIO io;
+    private WashingMessage wm;
+
 
     // TODO: add attributes
 
     public TemperatureController(WashingIO io) {
         // TODO
+        margin = 0.2;
+        this.io = io;
+        
+
     }
 
     @Override
     public void run() {
-        // TODO
+        try {
+            while (true){
+                WashingMessage wm = receiveWithTimeout(60000 / Settings.SPEEDUP);
+
+                
+
+
+            }
+            
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+
     }
 }
