3507a300e8788eb97347d63e788a10daebf2ab94
SamSap1 <150430881+SamSap1@users.noreply.github.com>
2024-10-08 22:13:05 +0200

    Update WaterController.java

--- wash/src/wash/control/WaterController.java
+++ wash/src/wash/control/WaterController.java
@@ -9,22 +9,17 @@ public class WaterController extends ActorThread<WashingMessage> {
 
     // TODO: add attributes
     private WashingIO io;
-    private double inputFlow = 0.1;
-    private double outputFlow = 0.2;
     private boolean filledCapacity;
     private boolean drainedComplete;
+    private boolean msgFlag;
     private ActorThread<WashingMessage> send;
     private double WaterLevel;
-
-    
-
-
-
+    private WashingMessage.Order currentMessage;
+    private WashingMessage oldMessage;
 
     public WaterController(WashingIO io) {
         // TODO
         this.io = io;
-
 
     }
 
@@ -32,101 +27,93 @@ public class WaterController extends ActorThread<WashingMessage> {
     public void run() {
         // TODO
         try {
-            while (true){
-                WashingMessage wm = receiveWithTimeout(500 / Settings.SPEEDUP);
+            while (true) {
+                WashingMessage newMessage = receiveWithTimeout(500 / Settings.SPEEDUP);
 
-                    if (wm != null){
+                if (newMessage != null) {
+                    if (currentMessage != null && currentMessage != newMessage.order()) {
+                        msgFlag = true;
 
+                    }
+                    oldMessage = newMessage;
+                    currentMessage = newMessage.order();
 
-                        switch (wm.order()) {
-                            case WATER_IDLE:
-                                    io.fill(false);
-                                    io.drain(false);
+                }
+                if (currentMessage != null) {
+
+                    switch (currentMessage) {
+                        case WATER_IDLE:
+                            io.fill(false);
+                            io.drain(false);
                             System.out.println("IDLE CHECK");
-                            wm.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+                            oldMessage.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
 
-                                break;
+                            break;
 
-                             case WATER_DRAIN:
-                             System.out.println("DRAIN CHECK");
+                        case WATER_DRAIN:
+                            System.out.println("DRAIN CHECK");
 
-                                drainCheck();
-                                wm.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+                            //double waterLevel = io.getWaterLevel();
 
-                                break;
+                            io.fill(false);
+                    
+                            if (io.getWaterLevel() > 0 && drainedComplete != true) {
+                                io.drain(true);
+                                drainedComplete = false;
+                    
+                            } else {
+                                io.drain(false);
+                                drainedComplete = true;
+                                if(msgFlag){
+                                oldMessage.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+                                msgFlag = false;
+                            }
+                            }
+                            break;
 
-                              case WATER_FILL:
-                              System.out.println("FILL CHECK");
+                        case WATER_FILL:
+                            System.out.println("FILL CHECK");
 
-                              waterFillCheck();
-                              wm.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+                           // double waterLevel = io.getWaterLevel();
 
-                              break;      
-               
-                        
-                            default:
-                                break;
-                        }
-    
+                            io.drain(false);
+
+                            if (io.getWaterLevel() < 10 && filledCapacity!=true) {
+                                io.fill(true);
+                                filledCapacity = false;
+                            } else {
+                                io.fill(false);
+                                filledCapacity = true;
+
+                                if (msgFlag){
+                                    oldMessage.sender().send(new WashingMessage(this, ACKNOWLEDGMENT));
+                                    msgFlag = false;
+                                }
+
+                            }
+
+                            break;
+
+                        default:
+                            break;
                     }
 
-                    
-                
-
+                }
 
             }
 
-
-            
         }
-        
-        catch (InterruptedException e){
+
+        catch (InterruptedException e) {
             System.out.println("WaterController");
         }
-        
+
         catch (Exception e) {
             throw new Error("sumting wong in watercontroller:" + e.getMessage());
-            // TODO: handle exception
         }
-
-
-
-    }
-    private void waterFillCheck(){
-        double waterLevel = io.getWaterLevel();
-
-        io.drain(false);
-
-
-        if (waterLevel < 10){
-            io.fill(true);
-            filledCapacity = false;
-        } else{
-            io.fill(false);
-            filledCapacity = true;
-
-        }
-
 
     }
 
-    private void drainCheck(){
-        double waterLevel = io.getWaterLevel();
-
-        io.fill(false);
-
-        if (waterLevel > 0){
-            io.drain(true);
-           drainedComplete = false;
-
-        }    else{
-            io.drain(false);
-            drainedComplete = true;
-
-        }
-
-
-    }
 
 
 
